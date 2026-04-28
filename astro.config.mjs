b762ec911fc7de63ab9f32ce3a3fcb8b203f10d9
Kim <me@kimdcottrell.com>
2026-04-28 06:02:38 +0000

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +20,6 @@ export default defineConfig({
   },
 
   vite: {
-    plugins: [tsconfigPaths({ loose: true }), tailwindcss()],
+    plugins: [tailwindcss()],
   },
 });
