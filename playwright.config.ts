dfaa96c0f3f6296f4fd3ae420df6a36d98d71976
Kim <me@kimdcottrell.com>
2026-05-09 00:07:52 +0000

    attempting to force e2e tests to run in pr

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,12 @@
 // NOTE:
 // this actually executes in a specialized `playwright` container
 // you can call it from the `app` container via `deno task e2e-test`
+import process from "node:process";
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   use: {
-    baseURL: "https://kanary.local.dev",
+    baseURL: process.env.BASE_URL ?? "https://kanary.local.dev",
     ignoreHTTPSErrors: true,
   },
   projects: [
